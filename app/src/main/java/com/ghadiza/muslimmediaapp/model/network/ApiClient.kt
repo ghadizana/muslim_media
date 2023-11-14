2e265e5dac71cded3e9e4c1b810ad006d37605cb
ghadizana <ghadizanauraaliya@gmail.com>
2023-11-14 09:45:52 +0700

    add comment

--- app/src/main/java/com/ghadiza/muslimmediaapp/model/network/ApiClient.kt
+++ app/src/main/java/com/ghadiza/muslimmediaapp/model/network/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
 // using object for creating single instance
-// for making request
+// for making request as client
 object ApiClient {
     fun provideApiService(): ApiService {
         val okHttpClient = OkHttpClient.Builder()
